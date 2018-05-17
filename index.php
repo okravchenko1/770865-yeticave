d0b359e1f13ba757a16ee50695087868c2a98d8c
okravchenko1 <kravchenko@pgups.ru>
2018-05-17 20:43:27 +0400

    Внесены изменения в соответствии с замечаниями

--- index.php
+++ index.php
@@ -120,29 +120,31 @@ $products = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php if (is_array($products)) { ?>
+                <?php foreach ($products as $key => $item) { ?>
+                    <?php if (isset($item['name'], $item['cat'], $item['price'], $item['url'])) { ?>
+                        <li class="lots__item lot">
+                            <div class="lot__image">
+                                <img src="<?= $item['url']; ?>" width="350" height="260" alt="<?= $item['name']; ?>">
+                            </div>
+                            <div class="lot__info">
+                                <span class="lot__category"><?= $item['cat']; ?></span>
+                                <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $item['name']; ?></a>
+                                </h3>
+                                <div class="lot__state">
+                                    <div class="lot__rate">
+                                        <span class="lot__amount">Стартовая цена</span>
+                                        <span class="lot__cost"><?= $item['price']; ?><b class="rub">р</b></span>
+                                    </div>
+                                    <div class="lot__timer timer">
 
-            <?php foreach ($products as $key => $item) { ?>
-                <?php if (is_array($item) && isset($item['name'], $item['cat'], $item['price'], $item['url'])) { ?>
-                    <li class="lots__item lot">
-                        <div class="lot__image">
-                            <img src="<?= $item['url']; ?>" width="350" height="260" alt="<?= $item['name']; ?>">
-                        </div>
-                        <div class="lot__info">
-                            <span class="lot__category"><?= $item['cat']; ?></span>
-                            <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $item['name']; ?></a></h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"><?= $item['price']; ?><b class="rub">р</b></span>
-                                </div>
-                                <div class="lot__timer timer">
-
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
+                        </li>
 
-                <?php };
+                    <?php };
+                };
             } ?>
         </ul>
     </section>
@@ -151,12 +153,14 @@ $products = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php foreach ($categories as $val) {
-                ?>
-                <li class="nav__item">
-                    <a href="all-lots.html"><?= $val; ?></a>
-                </li>
-            <?php } ?>
+            <?php if (is_array($categories)) { ?>
+                <?php foreach ($categories as $val) {
+                    ?>
+                    <li class="nav__item">
+                        <a href="all-lots.html"><?= $val; ?></a>
+                    </li>
+                <?php };
+            } ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
