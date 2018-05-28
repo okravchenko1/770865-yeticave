b0d0b81cc85c1fd5a411d603361357ee2cd485fe
okravchenko1 <kravchenko@pgups.ru>
2018-05-28 19:23:57 +0400

    испраавлено

--- templates/lot.php
+++ templates/lot.php
@@ -5,7 +5,7 @@
                 ?>
                 <?php if (isset($val['category_name'])) { ?>
                     <li class="nav__item">
-                        <a href="all-lots.html"><?= $val['category_name']; ?></a>
+                        <a href="all-lots.html"><?= htmlspecialchars($val['category_name']); ?></a>
                     </li>
                 <?php }
             }
@@ -13,15 +13,15 @@
     </ul>
 </nav>
 <section class="lot-item container">
-    <h2><?= $product['name'] ?></h2>
+    <h2><?= htmlspecialchars($product['name']) ?></h2>
     <div class="lot-item__content">
         <div class="lot-item__left">
             <div class="lot-item__image">
-                <img src="<?= $product['image']; ?>" width="730" height="548" alt="<?= $product['name'] ?>">
+                <img src="<?= $product['image']; ?>" width="730" height="548" alt="<?= htmlspecialchars($product['name']) ?>">
             </div>
             <p class="lot-item__category">Категория:
                 <span><?= htmlspecialchars($categories[$product['category_id'] - 1]['category_name']) ?></span></p>
-            <p class="lot-item__description"><?= $product['description'] ?></p>
+            <p class="lot-item__description"><?= htmlspecialchars($product['description']) ?></p>
         </div>
         <div class="lot-item__right">
             <div class="lot-item__state">
@@ -31,16 +31,16 @@
                 <div class="lot-item__cost-state">
                     <div class="lot-item__rate">
                         <span class="lot-item__amount">Текущая цена</span>
-                        <span class="lot-item__cost"><?= $userBet; ?></span>
+                        <span class="lot-item__cost"><?= htmlspecialchars($userBet); ?></span>
                     </div>
                     <div class="lot-item__min-cost">
-                        Мин. ставка <span><?= $userBet ?></span>
+                        Мин. ставка <span><?= htmlspecialchars($userBet) ?></span>
                     </div>
                 </div>
                 <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post">
                     <p class="lot-item__form-item">
                         <label for="cost">Ваша ставка</label>
-                        <input id="cost" type="number" name="cost" placeholder="<?=$userBet?>">
+                        <input id="cost" type="number" name="cost" placeholder="<?=htmlspecialchars($userBet)?>">
                     </p>
                     <button type="submit" class="button">Сделать ставку</button>
                 </form>
@@ -52,9 +52,9 @@
                     <?php foreach($bet as $val): ?>
                     <?php if (isset($val['user_name'], $val['bet_sum'], $val['date'])) { ?>
                         <tr class="history__item">
-                            <td class="history__name"><?=$val['user_name']; ?></td>
-                            <td class="history__price"><?=$val['bet_sum']; ?></td>
-                            <td class="history__time"><?=$val['date']; ?></td>
+                            <td class="history__name"><?=htmlspecialchars($val['user_name']); ?></td>
+                            <td class="history__price"><?=htmlspecialchars($val['bet_sum']); ?></td>
+                            <td class="history__time"><?=htmlspecialchars($val['date']); ?></td>
                         </tr><?php }?>
                     <?php endforeach ?>
                     <?php }?>
